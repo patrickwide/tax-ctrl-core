a0a08efb4779fe8f27f46b88625e1f67d42bbb4b
patrickwide <patrickwide254@gmail.com>
2026-08-25 10:51:44 +0300

    Setting up Flutter Rust Bridge hello world project

---
+++ src/lib.rs
@@ -0,0 +1,13 @@
+pub fn hello_core() -> String {
+    "Hello from tax_ctrl_core 🦀".to_string()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_hello_core() {
+        assert_eq!(hello_core(), "Hello from tax_ctrl_core 🦀");
+    }
+}
